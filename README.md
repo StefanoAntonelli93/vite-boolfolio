2da59232b9a135ccdf2e47aa7e73b182bd965caa
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-08-03 16:02:28 +0200

    feat: import axios,bootstrap + API in app.vue

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ Default scaffolding with sass in "devDependencies".
 ## for preview
 
 ctrl+shift+v
+
+---
+
+# VS Code Custom Config
+
+Modificare colori dell'area di lavoro
+
+## Vue
+
+{
+"workbench.colorCustomizations": {
+"titleBar.activeBackground": "#3FB27F",
+"titleBar.activeForeground": "#fff"
+}
+}
+
+## Laravel
+
+{
+"workbench.colorCustomizations": {
+"titleBar.activeBackground": "#ac3535",
+"titleBar.activeForeground": "#fff"
+}
+}
+
+---
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,50 @@
 <script>
+// importo axios
+import axios from "axios";
 
+export default {
+  name: "Projects",
+  data() {
+    return {
+      title: "Progetti",
+      projects: [],
+    };
+  },
+  methods: {
+    testLifeCycle() {
+      console.log("prova ciclo");
+    },
+    getProjects() {
+      const result = axios
+        .get("http://127.0.0.1:8000/api/projects")
+        .then((response) => {
+          console.log(response);
+          // se l'array è popolato restituisci qualcosa altrimenti messaggio errore
+          if (response.data.results.data.length) {
+            // console.log(response.data.results);
+            this.projects = response.data.results;
+          } else {
+            console.log("errore chiamata api");
+          }
+        })
+        .catch((error) => HTMLFormControlsCollection.log(error));
+    },
+  },
+  created() {
+    this.testLifeCycle();
+    this.getProjects();
+  },
+};
 </script>
 
 <template>
-  New Project
+  <header>header</header>
+  <main>
+    <div class="container">
+      <h1>{{ title }}</h1>
+    </div>
+  </main>
+  <footer>footer</footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
