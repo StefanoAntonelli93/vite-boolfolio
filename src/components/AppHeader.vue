0783d74a144dca0319b9a9b34f567b8f77ccad75
Stefano Antonelli <163558557+StefanoAntonelli93@users.noreply.github.com>
2025-10-16 14:15:30 +0200

    Change navbar brand text from 'Portfolio' to 'App Test'

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,7 +8,7 @@ export default {
   <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container">
-        <a class="navbar-brand" href="/">Portfolio</a>
+        <a class="navbar-brand" href="/">App Test</a>
         <button
           class="navbar-toggler"
           type="button"
